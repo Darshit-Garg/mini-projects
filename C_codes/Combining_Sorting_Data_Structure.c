a20d9e05597b3ed782c31174a455f1b3fd3370f8
Darshit-Garg <gargdarshit231@gmail.com>
2025-08-25 18:57:18 +0530

    Questions: Given  lists of students  of 2/4 CSE A and 2/4 CSE B in the sorted order of the marks find the 2/4 CSE student list in the sorted order of the student marks by combining the CSE-A and CSE-B marks list.

---
+++ C_codes/Combining_Sorting_Data_Structure.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+
+typedef struct
+{
+    char name[20];      //each item of list
+    char roll_no[30];
+    float marks;
+}Student_List;              
+
+void Bubble_Sort(Student_List [], int n);
+
+void Bubble_Sort(Student_List a[], int n)
+{
+    int i,j;
+    float temp;
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n-i-1; j++)
+        {
+            if(a[j].marks>a[j+1].marks)
+            {
+                temp = a[j].marks;
+                a[j].marks = a[j+1].marks;
+                a[j+1].marks = temp;
+            }
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int nA,nB,temp;
+    int i,j,k;
+    printf("Enter the number of students in CSE A: ");
+    scanf("%d",&nA);
+    printf("Enter the number of students in CSE B: ");
+    scanf("%d",&nB);
+
+    Student_List CSE_A[nA];
+    Student_List CSE_B[nB];
+    Student_List CSE[nA+nB];
+
+    printf("\nEnter CSE A student details: ");
+    for(i = 0; i < nA; i++)
+    {
+        printf("Student %d\n",i+1);
+        printf("Name: ");
+        scanf("%*c%[^\n]%*c",&CSE_A[i].name);
+        printf("Roll No: ");
+        scanf("%[^\n]%*c",&CSE_A[i].roll_no);
+        printf("Marks: ");
+        scanf("%f",&CSE_A[i].marks);
+        printf("\n");
+    }
+
+    printf("Enter CSE B student details: ");
+    for(i = 0; i < nB; i++)
+    {
+        printf("Student %d\n",i+1);
+        printf("Name: ");
+        scanf("%*c%[^\n]%*c",&CSE_B[i].name);
+        printf("Roll No: ");
+        scanf("%[^\n]%*c",&CSE_B[i].roll_no);
+        printf("Marks: ");
+        scanf("%f",&CSE_B[i].marks);
+        printf("\n");
+    }
+
+    Bubble_Sort(CSE_A,nA);
+    Bubble_Sort(CSE_B,nB);
+
+    k = 0;
+
+    // for(i = 0; i < nA; i++)
+    // {
+    //     for(j = temp; j < nB; j++)
+    //     {
+    //         if(CSE_A[i].marks>CSE_B[j].marks)
+    //         {
+    //             CSE[k] = CSE_B[j];
+    //             //printf("In if %.1f\n",CSE[k].marks);
+    //             k++;
+    //         }
+    //         else
+    //         {
+    //             CSE[k] = CSE_A[i];
+    //             //printf("In else %.1f\n",CSE[k].marks);
+    //             temp = j;
+    //             k++;
+    //             break;
+    //         }
+    //     }
+    // }
+
+    printf("The final list of CSE students: \n");
+    for(i = 0; i < (nA+nB); i++)
+    {
+        printf("%s\t%s\t%.1f\n",CSE[i].name,CSE[i].roll_no,CSE[i].marks);
+        //printf("%.1f\n",CSE[i].marks);
+    }
+    return 0;
+}
