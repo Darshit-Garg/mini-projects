09d48dc77b82eabd07c46f1ce38257594d642a03
Darshit-Garg <gargdarshit231@gmail.com>
2025-08-29 00:45:42 +0530

    Given a set of points, make a basket with lowest laying points to make it a basket

---
+++ C_codes/FruitBasket.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<math.h>
+
+typedef struct
+{
+	int x;
+    int y;
+}point;
+
+int main()
+{
+	int N;
+    scanf("%d",&N);
+    point points[N];
+    for(int i = 0; i < N; i++)
+    {
+    	scanf("%d%d",&points[i].x,&points[i].y);
+	}
+    int X;
+    X = points[0].x;
+    point min[N];
+    min[0] = points[0];
+    int j = 0;
+    int count = 1;
+    for(int i = 0; i<N; i++)
+    {
+    	if(points[i].x == X)
+        {
+        	if(min[j].y > points[i].y)
+            {
+            	min[j] = points[i];
+            }
+        }
+        else
+        {
+        	count++;
+        	j++;
+            X = points[i].x;
+        }
+	}
+    
+    float perimeter;
+    
+	for(int i = 0; i < count-1; i++)
+    {
+    	perimeter = sqrt((min[i+1].x - min[i].x)^2 + (min[i+1].y - min[i].y)^2);
+    }
+    int Perimeter = roundf(perimeter);  
+    printf("%d",Perimeter);
+    return 0;
+}
