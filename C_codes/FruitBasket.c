e4edd13b8f2342d63014f5d4eaf4f6323b86aa5e
Darshit-Garg <gargdarshit231@gmail.com>
2025-09-08 22:52:28 +0530

    Fixed the garbage value bug

--- C_codes/FruitBasket.c
+++ C_codes/FruitBasket.c
@@ -51,34 +51,47 @@ int main()
     //     printf("%d %d\n",points[i].x,points[i].y);
     // }
     X = points[0].x;
+    // printf("%d\n",X);
+
     point min[N];
     min[0] = points[0];
+
+    // printf("%d %d\n",min[0].x,min[0].y);
+
+    // No mistake till here
+
     int j = 0;
     int count = 1;
+
     for(int i = 0; i<N; i++)
     {
+        //printf("i = %d\n",i);
     	if(points[i].x == X)
         {
+        //    printf("In first main if for i = %d\n",i);
         	if(min[j].y > points[i].y)
             {
-            	min[j] = points[i];
+                min[j] = points[i];
+        //        printf("min y = %d\n",min[j].y);
             }
+        //    printf("min[%d] = %d\n",j,min[j].y);
         }
         else
         {
-        	count++;
-        	j++;
-            X = points[i].x;
+        	count++;                
+            j++;
+            X = points[i--].x;
+        //    printf("%d %d\n",i,X);
         }
 	}
-    
-    float perimeter;
+
+    float Perimeter = 0;
     
 	for(int i = 0; i < count-1; i++)
     {
-    	perimeter = sqrt((min[i+1].x - min[i].x)^2 + (min[i+1].y - min[i].y)^2);
+    	Perimeter += sqrt((min[i+1].x - min[i].x)*(min[i+1].x - min[i].x) + (min[i+1].y - min[i].y)*(min[i+1].y - min[i].y));
     }
-    int Perimeter = roundf(perimeter);  
-    printf("%d",Perimeter);
+    int perimeter = roundf(Perimeter);  
+    printf("%d",perimeter);
     return 0;
 }
